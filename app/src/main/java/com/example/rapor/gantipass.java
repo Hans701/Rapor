d2c1d5c1b32cf9911951a3956bb0c594abc18d7f
M. Raihan Dinul Haq <Hans701.mraihandinulhaq@gmail.com>
2021-07-22 23:08:25 +0700

    add layout

--- app/src/main/java/com/example/rapor/gantipass.java
+++ app/src/main/java/com/example/rapor/gantipass.java
@@ -2,13 +2,25 @@ package com.example.rapor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class gantipass extends AppCompatActivity {
+    Button kembali, ganti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gantipass);
+
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dashboardIntent = new Intent(gantipass.this, dashboard.class);
+                startActivity(dashboardIntent);
+            }
+        });
     }
 }
