49332251a813a1a1438fab16bf2847def3db4db3
M. Raihan Dinul Haq <Hans701.mraihandinulhaq@gmail.com>
2021-07-22 14:30:44 +0700

    add layout

--- app/src/main/java/com/example/rapor/dashboard.java
+++ app/src/main/java/com/example/rapor/dashboard.java
@@ -2,13 +2,36 @@ package com.example.rapor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class dashboard extends AppCompatActivity {
+    Button profil, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        profil = (Button) findViewById(R.id.profil);
+        logout = (Button) findViewById(R.id.logout);
+
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profilIntent = new Intent(dashboard.this, profil.class);
+                startActivity(profilIntent);
+            }
+        });
+
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profilIntent = new Intent(dashboard.this, login.class);
+                startActivity(profilIntent);
+            }
+        });
     }
 }
