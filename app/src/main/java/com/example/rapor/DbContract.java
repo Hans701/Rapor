329c2d9c897271d94d3b6c407721ceb8162797bf
M. Raihan Dinul Haq <Hans701.mraihandinulhaq@gmail.com>
2021-07-26 00:27:40 +0700

    bug login

--- app/src/main/java/com/example/rapor/DbContract.java
+++ app/src/main/java/com/example/rapor/DbContract.java
@@ -1,5 +1,5 @@
 package com.example.rapor;
 
 public class DbContract {
-    public static final String SERVER_LOGIN_URL = "http://192.168.1.6/Login/ceklogin.php";
+    public static final String SERVER_LOGIN_URL = "http://192.168.1.8/clogin/login.php";
 }
